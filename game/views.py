7b44baea8d129d990009d31fce9b15a799ba68c1
Payton Quinn <paytonq@uw.edu>
2016-03-03 23:43:10 -0800

    use session_key

--- game/views.py
+++ game/views.py
@@ -1,18 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
-sessionid = 0;
 
 def index(request):
-    global sessionid
-    if 'idSet' not in request.session:
-        sessionid += 1
-        request.session['sessionid'] = sessionid
-        request.session['idSet'] = 1
+    request.session['idSet'] = 1
     return render_to_response('index.html', {'verify': 'Testing Django Views Successful!'})
 
 def hello(request):
     return HttpResponse('Ajax Successful!')
 
 def getsessionid(request):
-    return HttpResponse(request.session['sessionid'])
+    return HttpResponse(request.session._session_key)
